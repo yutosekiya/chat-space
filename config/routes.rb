a577a61d883ea94c8413dbccd85b300977a66f01
関谷佑斗 <yhk3165t@gmail.com>
2018-06-07 13:09:47 +0900

    [Edit]Routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
-  resources :messages, only: [:index]
+  root 'groups#index'
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
+
 end
