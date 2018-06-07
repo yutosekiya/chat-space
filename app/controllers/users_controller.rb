4aa4173713db379e64554f1a4791256c37cb245c
関谷佑斗 <yhk3165t@gmail.com>
2018-06-07 10:25:46 +0900

    Make Flashmessage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
   def update
     if current_user.update(user_params)
-      refirect_to root_path
+      redirect_to root_path
     else
       render :edit
     end
