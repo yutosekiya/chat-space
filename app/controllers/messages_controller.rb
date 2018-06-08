2f61e67b34185f635b40aa099fc9eb910d7f052d
関谷佑斗 <yhk3165t@gmail.com>
2018-06-08 18:27:37 +0900

    [Edit]messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,16 @@
 class MessagesController < ApplicationController
   def index
+    @messages = Message.order('id ASC')
   end
 
   def create
+    Message.create(message_params)
   end
+
+  private
+  def message_params
+    params.require(:message).permit(:text, :image).merge(params[:user_id, :group_id])
+  end
+
 
 end
