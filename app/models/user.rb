fa4c59f1ea84c350f976bf50ce2a0600cbd92f83
関谷佑斗 <yhk3165t@gmail.com>
2018-06-08 18:01:05 +0900

    [Edit models] add association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :groups, through: :group_users
   has_many :group_users
+  has_many  :messages
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
