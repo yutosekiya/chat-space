e0274fb7e26f138613e7c94381577bf628d5d524
関谷佑斗 <yhk3165t@gmail.com>
2018-06-09 11:27:55 +0900

    [Fix]messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,7 +9,7 @@ class MessagesController < ApplicationController
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_message_path(@group), notice: 'メッセージが送信されました'
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください'
