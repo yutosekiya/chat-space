fc638750be09c924ac7cb460910b47050fae73aa
関谷佑斗 <yhk3165t@gmail.com>
2018-06-15 23:59:10 +0900

    チャットメンバーにログインしているユーザーが表示されないようにする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-     @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+     @users = User.where('name LIKE(?) and id != ? ', "%#{params[:keyword]}%", current_user.id)
      respond_to do |format|
       format.html
       format.json
