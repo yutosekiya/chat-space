13b287e506d28811e62321452f65bbec4601d7d7
関谷佑斗 <yhk3165t@gmail.com>
2018-06-15 10:29:15 +0900

    groups_controllerの誤りを修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,7 +21,7 @@ class GroupsController < ApplicationController
   def edit
   end
 
-  def udpate
+  def update
     if  @group.update(group_params)
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
     else
