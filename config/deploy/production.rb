6de6b59a1a4cd4149aed486ccb8af7251f97ee0b
関谷佑斗 <yhk3165t@gmail.com>
2018-06-20 09:52:38 +0900

    デプロイ作業　config/deploy.rbとdeploy/production.rbを編集

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '13.230.210.180', user: 'ec2-user', roles: %w{app db web}
+server '13.230.234.134', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
